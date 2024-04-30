4ec02a5cd3a6c6d9abb6c0f593f398a8c04cbbdf
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-04-30 20:06:34 -0300

    Criação de estilos globais e tem padrão

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './global.css';
+import { ThemeProvider } from 'styled-components';
 import { AboutUs } from './pages/AboutUs';
 import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
+import { Page404 } from './pages/Page404';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
     return (
-		<>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/aboutUs" element={<AboutUs />} />
 					<Route path="/contact" element={<Contact />} />
+					<Route path="*" element={<Page404 />} />
 				</Routes>
 			</BrowserRouter>
 
@@ -29,6 +34,6 @@ export function App() {
 				pauseOnHover
 				theme="dark"
 			/>
-		</>
+		</ThemeProvider>
     )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['text-transition-green']};
+    }
+
+    body {
+        background-color: ${props => props.theme['dark']};
+        color: ${props => props.theme['text-gray']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font: 400 16px 'Roboto', sans-serif;
+    }
+
+
+	h1 {
+		font-size: 2rem;
+		font-weight: bold;
+		margin-bottom: 1rem;
+		color: ${props => props.theme.white};
+	}
+
+	p {
+		font-weight: 400;
+		color: #e0e0e0;
+	}
+`;
+
+// * {
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+// }
+
+// body {
+// 	font-family: 'Roboto', sans-serif;
+// 	background: #171717;
+
+// 	color: white;
+// }
+
+// h1 {
+// 	font-size: 2rem;
+//     font-weight: bold;
+//     margin-bottom: 1rem;
+// }
+
+// p {
+// 	font-weight: 400;
+//     color: #e0e0e0;
+// }
