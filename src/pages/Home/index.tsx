a137801e8100dd9d3a0d6dc207290aa10d67f9af
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-04-30 21:46:09 -0300

    Styled components na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import bgImg from '../../assets/images/bg-home-image.png';
 import { Header } from '../../components/Header';
-import './style.css';
+import { HomeContainer } from './styles';
 
 export function Home() {
     return (
         <div>
             <Header />
-			<main>
+			<HomeContainer>
 				<div className="description">
 					<h1>Empowering You in the Digital Age</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum congue metus quis accumsan euismod. Maecenas sed est mollis, convallis nisi convallis, imperdiet massa. Proin ipsum nunc, lacinia ac faucibus quis, ullamcorper non metus. </p>
@@ -14,7 +14,7 @@ export function Home() {
 				<div className='image-container'>
 					<img src={bgImg} className='image'/>
 				</div>
-			</main>
+			</HomeContainer>
         </div>
     )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	padding: 1rem;
+
+	margin: 8rem 1rem;
+
+	h1 {
+		font-size: 5rem;
+	}
+
+	p {
+		font-size: 1.5rem;
+	}
+
+	div .image {
+		width: 530px;
+		height: 530px;
+		margin-right: 130px;
+	}
+
+	.description {
+		width: 675px;
+	}
+`;
