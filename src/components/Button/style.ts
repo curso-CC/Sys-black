133c0b30b4b01c5908baf051fee0e9db72b182bc
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-05-03 19:12:06 -0300

    Mudança de cores do botão

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -3,12 +3,13 @@ import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
 	width: 50%;
-	padding: 15px 20px;
-	background-color: ${props => props.theme["text-green"]};
+	padding: 20px 20px;
+	background-color: ${props => props.theme["text-transition-green"]};
 	color: white;
 	border: none;
 	border-radius: 6px;
 	cursor: pointer;
+	font-weight: bold;
 
 	transition: background-color 200ms linear;
 
