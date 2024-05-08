d7249b5bff15f14b7b26349bb4afeaf3da4ee8c7
Sidney Cardoso <sidneyrsc342@gmail.com>
2024-05-08 16:18:56 -0300

    Menu responsivo

---
+++ scripts/nav.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    menu.classList.toggle('open');
+});
